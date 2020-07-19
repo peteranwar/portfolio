4fc014003df7dfd0e04ebf287374a530b8064fd2
Your Name <you@example.com>
2020-07-20 01:00:34 +0200

    gg

---
+++ main.js
@@ -0,0 +1,36 @@
+//  Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// set initial state of MENU 
+let showMenu = false;
+
+ menuBtn.addEventListener('click', toggleMenu);
+
+ function toggleMenu(){
+     if (!showMenu) {
+         menuBtn.classList.add('close');
+         menu.classList.add('show');
+         menuNav.classList.add('show');
+         menuBranding.classList.add('show');
+         navItems.forEach( navItem => navItem.classList.add('show'));
+
+
+        //  set menu state 
+        showMenu =true;
+     } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach( navItem => navItem.classList.remove('show'));
+
+        //  set menu state
+        showMenu = false;
+     }
+    
+
+ }
